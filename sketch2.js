754257f0d867da5603247bd70624c53a39538ef5
Jakob <jakob.kirner@tutanota.com>
2023-10-28 17:06:27 +0200

    Work in Progress

--- sketch2.js
+++ sketch2.js
@@ -1,34 +1,54 @@
 let sizex = 24;
 let sizey = 18;
 let tilesize = 40;
+let map = [];
+let Solar;
+let Select;
+let Wiese;
 
 function setup() {
   createCanvas(sizex * tilesize, sizey * tilesize);
-  Solar = loadImage('Solar.png');
+
+  Solar = loadImage("Solar.png");
+  Select = loadImage("Select.png");
+  Wiese = loadImage("Wiese.png");
+
   map = new Array();
-  
+
   for (let y = 0; y < sizey; y++) {
     for (let x = 0; x < sizex; x++) {
-    	
+      map.push(new map_tile(Wiese));
     }
   }
 }
 
 function draw() {
-  background(220);
-
   for (let y = 0; y < sizey; y++) {
     for (let x = 0; x < sizex; x++) {
-    
       //rect(x * tilesize, y * tilesize, tilesize, tilesize);
-      //image(Solar, x * tilesize, y * tilesize, tilesize, tilesize);
+      image(
+        map[x + y * sizex].image,
+        x * tilesize,
+        y * tilesize,
+        tilesize,
+        tilesize
+      );
+    }
+  }
+
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    x = parseInt(mouseX / tilesize);
+    y = parseInt(mouseY / tilesize);
+    image(Select, x * tilesize, y * tilesize, tilesize, tilesize);
+    if (mouseIsPressed) {
+      map[x + y * sizex].image = Solar;
     }
   }
 }
 
-class map_tile () {
-	constructor(image) {
-	this.image = image;
-	console.log(image);
-	}	
+class map_tile {
+  constructor(image) {
+    this.image = image;
+  }
 }
+
