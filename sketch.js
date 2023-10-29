d52e2e02b7118c2645965623af1334b8da6d3885
Theodore Servatius <tservatius2020@brgkepler.at>
2023-10-29 10:16:18 +0100

    letzte aenderungen

--- sketch.js
+++ sketch.js
@@ -5,24 +5,26 @@ let wald
 let geradeSchiene
 var Hintergrund = false
 let solarkraftwerk
+let tileSize = window.innerWidth/20
 
 function preload() {
   wiese = loadImage("klimathon-wiese-upscale.png");
-  wueste = loadImage("klimathon-wueste.png");
+  wueste = loadImage("klimathon-wueste-upscale.png");
   waldUpscaled = loadImage("klimathon-wald-upscaled.png");
   wald = loadImage("klimathon-wald.png");
   geradeSchiene = loadImage("klimathon-zugschiene-gerade-upscaled.png");
   solarkraftwerk = loadImage("klimathon-solarkraftwerk-schwarz-grau.png")
 }
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  for(imgY=0; imgY<window.innerHeight; imgY+=80) {
-    for(imgX=0; imgX<window.innerWidth; imgX+=80) {
-      image(waldUpscaled, imgX, imgY, 80, 80);
-      console.log(imgX)
+  for(imgY=0; imgY<window.innerHeight; imgY+=tileSize) {
+    for(imgX=0; imgX<window.innerWidth; imgX+=tileSize) {
+      image(wiese, imgX, imgY, tileSize, tileSize);
     }
   }
 }
+
 
 function draw() {
   if(Hintergrund == false){
@@ -30,10 +32,10 @@ function draw() {
     Hintergrund = true
   }
   //image(wald, mouseX-10, mouseY-10)
-  image(wiese, parseInt(mouseX/80)*80, parseInt(mouseY/80)*80, 80, 80)
   if(mouseIsPressed) {
     //image(waldUpscaled, parseInt(mouseX/80)*80, parseInt(mouseY/80)*80, 80, 80)
     //image(wueste, mouseX-10, mouseY-10, 80, 80)
-    image(solarkraftwerk, parseInt(mouseX/80)*80, parseInt(mouseY/80)*80, 80, 80)
+    image(wueste, parseInt(mouseX/tileSize)*tileSize, parseInt(mouseY/tileSize)*tileSize, tileSize, tileSize)
   }
+  image(solarkraftwerk, parseInt(mouseX/tileSize)*tileSize, parseInt(mouseY/tileSize)*tileSize, tileSize, tileSize)
 }
